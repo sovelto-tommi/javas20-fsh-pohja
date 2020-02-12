2b5f54736471f320b3ca54eb1154b49ea539242a
Tommi Teräsvirta <tommi.terasvirta@sovelto.fi>
2020-02-12 20:54:03 +0200

    Initial commit

---
+++ src/main/java/fi/academy/JavaS20FshPohjaApplication.java
@@ -0,0 +1,13 @@
+package fi.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaS20FshPohjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaS20FshPohjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fi/academy/JavaS20FshPohjaApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaS20FshPohjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
